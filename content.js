c9a8f8c5e4bb039866fb1a9368b4ed682d4c36f9
uridvir <uri.j.dvir@gmail.com>
2019-08-12 21:57:01 -0400

    Added export button

--- content.js
+++ content.js
@@ -1,2 +1,20 @@
 //TODO add actual code
-alert("Hello world!")
+
+var dataDisplayTable = document.getElementsByClassName('datadisplaytable')
+
+if (dataDisplayTable != null){
+    window.exportAction = function() {
+        alert("exportAction called!")
+    }
+
+    var button = document.createElement("input")
+    button.id = "export_button"
+    button.type = "button"
+    button.value = "Export to CSV"
+    button.onclick = window.exportAction
+    button.style.width = "100%"
+    button.style.height = "100px"
+    button.style.fontSize = "32pt"
+    button.style.backgroundColor = "#003366"
+    document.body.insertBefore(button, document.body.children[0])
+}
