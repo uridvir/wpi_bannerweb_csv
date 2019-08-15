4d54cf0368c8a8f6ed9a3770a1052b4826816467
uridvir <uri.j.dvir@gmail.com>
2019-08-14 20:42:36 -0400

    Moved imports.js

---
+++ imports.js
@@ -0,0 +1,10 @@
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse){
+        if (request.greeting == 'imports'){
+            //Add more imports here
+            chrome.tabs.executeScript({file: 'lib/schedule_changes.js'},
+                chrome.tabs.executeScript({file: 'lib/course.js'}, sendResponse({farewell: ''}))
+            )
+        }
+    }
+)
